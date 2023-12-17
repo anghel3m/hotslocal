b75638319e5d9027eb7a1f24216846002935f628
anghel3m <adms95@outlook.es>
2023-12-17 12:49:31 -0500

    se crea la logica del combate

--- funcion.js
+++ funcion.js
@@ -1,3 +1,85 @@
 let nombre = ""
-    nombre = prompt("Cuál es tu nombre?")
-             alert("Bienvenido " + nombre )
+nombre = prompt("Cuál es tu nombre?")
+alert("Bienvenido " + nombre)
+
+
+
+
+function aleatorio(max, min) {
+    return Math.floor(Math.random() * (max - min + 1) + 1);
+}
+
+function eleccion(jugada) {
+
+    let resultado = "";
+
+    if (jugada == 1) {
+        resultado = "Piedra";
+
+    } else if (jugada == 2) {
+        resultado = "Papel";
+
+    } else if (jugada == 3) {
+        resultado = "Tijera";
+
+    } else {
+        resultado = "elccion incorrecta";
+    }
+
+    return resultado
+}
+
+function combate (jugador,pc){
+
+    let resultadoCombate = "";   
+    
+
+    if (jugador == pc) {
+        resultadoCombate = "Empataron";
+
+    } else if (jugador == 1 && pc == 3) {
+        resultadoCombate ="Ganaste";
+        
+
+    } else if (jugador == 2 && pc == 1) {
+        resultadoCombate ="Ganaste";
+        
+
+    } else if (jugador == 3 && pc == 2) {
+        resultadoCombate ="Ganaste";
+        
+    } else {
+        resultadoCombate ="Perdiste";
+        
+    }
+    return resultadoCombate 
+
+
+    
+}
+
+let jugador = 0;
+let pc = 0;
+let ganadas = 0;
+let perdidas = 0;
+
+
+while (ganadas < 3 && perdidas < 3) {
+
+    jugador = prompt("1 es piedra, 2 es papel, 3 es tijera")
+
+    pc = aleatorio(3, 1)
+
+    alert("PC elige " + eleccion(pc) + " y tu elegiste " + eleccion(jugador))
+
+    let resultado = combate(jugador,pc);
+    if (resultado == "Ganaste"){
+        ganadas =ganadas+1;
+    }else if (resultado == "Perdiste"){
+        perdidas=perdidas+1
+
+    }
+
+
+alert("Ganaste "+ ganadas + "veces. " + "perdiste " + perdidas + " veces.")
+}
